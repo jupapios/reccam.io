5aeb83e05d15422b401d4fff2b33f338395e0f5e
Juan Pablo Pinilla Ossa <gotik@users.noreply.github.com>
2016-09-23 15:26:12 -0500

    Enh tests adding enzyme and sinon (#14)
    - `enzyme` gives a better API to interact with the rendered components
    - `sinon` allows to spy without the complexity of jest or exporting
    the spies from the modules

--- test/App.test.js
+++ test/App.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import App from '../src/App';
+import sinon from 'sinon';
+import { mount } from 'enzyme';
+import { mountToJson } from 'enzyme-to-json';
 import Deferred from './utils/Deferred';
 import NavigatorUserMediaError from './utils/NavigatorUserMediaError';
-
-import { MediaRecorderSpy } from './utils/MediaRecorder';
-import { createObjectURLSpy } from './utils/createObjectURL';
 
 describe('App', () => {
   let deferred;
@@ -14,38 +13,43 @@ describe('App', () => {
   beforeEach(() => {
     deferred = new Deferred();
     navigator.mediaDevices = { getUserMedia: constraints => deferred.promise };
-    component = renderer.create(<App />);
+    component = mount(<App />);
   });
 
-  it('renders null while loading', () => {
-    expect(component.toJSON()).toMatchSnapshot();
+  it('renders empty while loading', () => {
+    expect(mountToJson(component)).toMatchSnapshot();
   });
 
-  it('renders the video URL from the stream', async () => {
-    const stream = new MediaStream();
-    deferred.resolve(stream);
-
-    await deferred.promise;
-    expect(createObjectURLSpy).lastCalledWith(stream);
-    expect(component.toJSON()).toMatchSnapshot();
-  });
-
-  it('renders null when media access is denied', async () => {
+  it('renders empty when media access is denied', async () => {
     const error = new NavigatorUserMediaError();
     deferred.reject(error);
 
     try {
       await deferred.promise;
     } catch(e) {
-      expect(component.toJSON()).toMatchSnapshot();
+      expect(mountToJson(component)).toMatchSnapshot();
     }
   });
 
-  it('initializes the MediaRecorder', async () => {
+  it('renders the video URL from the stream', async () => {
+    const videoURL = 'http://localhost/e75dbc24-dd2c-4157-bf0e-f0a7773e98c8';
+    sinon.stub(URL, 'createObjectURL', stream => videoURL);
+
     const stream = new MediaStream();
     deferred.resolve(stream);
 
     await deferred.promise;
-    expect(MediaRecorderSpy).lastCalledWith(stream, { mimeType: 'video/webm;codecs=vp9' });
+    expect(URL.createObjectURL.calledWith(stream));
+    expect(mountToJson(component)).toMatchSnapshot();
+  });
+
+  it('initializes the MediaRecorder', async () => {
+    sinon.spy(global, 'MediaRecorder');
+
+    const stream = new MediaStream();
+    deferred.resolve(stream);
+
+    await deferred.promise;
+    expect(MediaRecorder.calledWith(stream, { mimeType: 'video/webm;codecs=vp9' }));
   });
 });
