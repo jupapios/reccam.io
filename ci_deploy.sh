4f6947ae4248a4cf441de5006abb564537238c7e
Juan Pablo Pinilla Ossa <gotik@users.noreply.github.com>
2016-09-25 21:33:53 -0500

    Add continous integration

---
+++ ci_deploy.sh
@@ -0,0 +1,19 @@
+now=$(date +"%m-%d-%y %H:%M:%S")
+commit_msg="Deployment from CircleCI $now"
+current_branch="$(git rev-parse --abbrev-ref HEAD)"
+
+echo "Deploying branch $current_branch ($commit_msg)"
+
+compass compile
+npm run build
+
+git config user.name "CircleCI"
+git config user.email "circleci@reccam.io"
+
+git checkout -B gh-pages
+git add -f build
+git commit -am "$commit_msg"
+git checkout .
+git filter-branch -f --prune-empty --subdirectory-filter build
+git push -f origin gh-pages
+git checkout -
