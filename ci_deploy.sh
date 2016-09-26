2114e6c687f06edd04733e35ecb2197e31610859
Juan Pinilla Ossa <juan@kamikazepanda.com>
2016-09-25 22:31:26 -0500

    Update ci_deploy.sh

--- ci_deploy.sh
+++ ci_deploy.sh
@@ -7,8 +7,11 @@ echo "Deploying branch $current_branch ($commit_msg)"
 compass compile
 npm run build
 
+git config user.name "CircleCI"
+git config user.email "circleci@reccam.io"
+
 git checkout -B gh-pages
-git add -f build
+git add -f --all
 git commit -am "$commit"
 git filter-branch -f --prune-empty --subdirectory-filter build
 git push -f origin gh-pages
