55f6c15192ba8c9b3296106ab4984b395791a4b1
gotik <juan@kamikazepanda.com>
2016-09-27 22:59:44 -0500

    Fix mobile constraints

--- src/config/media-constraints.js
+++ src/config/media-constraints.js
@@ -1,7 +1,16 @@
-export default {
+import isMobile from 'ismobilejs';
+
+const mobile = {
+  audio: true,
+  video: true,
+};
+
+const desktop = {
   audio: true,
   video: {
     height: { ideal: 720 },
     width: { ideal: 1280 },
   },
 };
+
+export default isMobile.any ? mobile : desktop;
